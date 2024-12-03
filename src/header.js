6919241b39284071cdbab5a883c262d87b4ff8fc
Kiral-jessie <udejicynthia@gmail.com>
2024-12-03 23:53:52 +0100

    Update header.js

--- src/header.js
+++ src/header.js
@@ -10,7 +10,7 @@ function Header({ setView }) {
           <li><Link to="/contact">Contact</Link></li>
         </ul>
   <header className="header">
-      <h3>Click The Button To Either Register or Reserve A Space(you can reserve a spot without registering with us but registered users are proritized first for reservation spot)</h3>
+      <h3>Click The Button To Either Register or Reserve A Space(you can reserve a spot without registering with us but registered users are prioritized first for reservation spot)</h3>
       <nav className="nav">
         <button onClick={() => setView('register')}>Register</button>
         <button onClick={() => setView('reserve')}>Reserve</button>
